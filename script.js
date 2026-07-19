e466b751e781abb72c2d79e9751d465163da00c1
Rajan KC <rajankckc7@gmail.com>
2026-07-19 16:54:14 +0000

    Add Notes app functionality with UI elements and styles

--- script.js
+++ script.js
@@ -1,65 +1,130 @@
-dragElement(document.getElementsByClassName("window")[0]);
+var biggestIndex = 1;
+var selectedIcon = undefined;
+
+function closeWindow(element) {
+    if (element) element.style.display = "none";
+}
+
+// Fixed function to bring window to front when opened or clicked
+function openWindow(element) {
+    if (!element) return;
+    element.style.display = "flex";
+    biggestIndex++;
+    element.style.zIndex = biggestIndex;
+}
+
+function handleWindowTap(element) {
+    biggestIndex++;
+    element.style.zIndex = biggestIndex;
+    if (selectedIcon) {
+        deselectIcon(selectedIcon);
+    }
+}
+
+function addWindowTapHandling(element) {
+    if(element) {
+        element.addEventListener("mousedown", () => handleWindowTap(element));
+    }
+}
+
+function selectIcon(element) {
+    element.classList.add("selected");
+    selectedIcon = element;
+}
+
+function deselectIcon(element) {
+    if(element) {
+        element.classList.remove("selected");
+    }
+    selectedIcon = undefined;
+}
+
+function unlockOS(e) {
+    e.preventDefault();
+    var welcomeScreen = document.querySelector("#welcome");
+    closeWindow(welcomeScreen);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    var welcomeScreen = document.querySelector("#welcome");
+    var welcomeScreenClose = document.querySelector("#welcomeclose");
+    var welcomeScreenOpen = document.querySelector("#welcomeopen");
+    
+    var notesScreen = document.querySelector("#notes");
+    var notesScreenClose = document.querySelector("#notesclose");
+    var notesIcon = document.querySelector("#notesicon"); // Hook up the entire app icon container
+
+    if (welcomeScreen) {
+        dragElement(welcomeScreen);
+        addWindowTapHandling(welcomeScreen);
+    }
+
+    if (notesScreen) {
+        dragElement(notesScreen);
+        addWindowTapHandling(notesScreen);
+    }
+
+    // Opens Notes when clicking the icon
+    if (notesIcon && notesScreen) {
+        notesIcon.addEventListener("click", (e) => {
+            openWindow(notesScreen);
+        });
+    }
+
+    if (welcomeScreenClose && welcomeScreen) {
+        welcomeScreenClose.addEventListener("click", (e) => {
+            e.stopPropagation();
+            closeWindow(welcomeScreen);
+        });
+    }
+
+    if (welcomeScreenOpen && welcomeScreen) {
+        welcomeScreenOpen.addEventListener("click", () => {
+            openWindow(welcomeScreen);
+        });
+    }
+
+    if (notesScreenClose && notesScreen) {
+        notesScreenClose.addEventListener("click", (e) => {
+            e.stopPropagation();
+            closeWindow(notesScreen);
+        });
+    }
+});
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementsByClassName(elmnt.id + "header")[0]) {
-    // if present, the header is where you move the DIV from:
-    document.getElementsByClassName(elmnt.id + "header")[0].onmousedown = dragMouseDown;
+  var targetHeader = document.getElementById(elmnt.id + "header");
+  
+  if (targetHeader) {
+    targetHeader.onmousedown = dragMouseDown;
   } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
   }
 
   function dragMouseDown(e) {
     e = e || window.event;
+    if (e.target.className === "close" || e.target.className === "minimize" || e.target.className === "maximize") return;
     e.preventDefault();
-    // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
   }
 
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
-    // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement() {
-    // stop moving when mouse button is released:
     document.onmouseup = null;
     document.onmousemove = null;
   }
-}
-
-function closeWindow(element) {
-  element.style.display = "none";
-}
-
-function openWindow(element) {
-  element.style.display = "flex";
-}
-
-var welcomeScreenClose = document.querySelector("#welcomeclose")
-var welcomeScreenOpen = document.querySelector("#welcomeopen")
-
-if (welcomeScreenClose && welcomeScreenOpen) {
-welcomeScreenClose.addEventListener("click", function() {
-  closeWindow(welcomeScreen);
-});
-}
-
-if (welcomeScreenOpen && welcomeScreen) {
-welcomeScreenOpen.addEventListener("click", function() {
-  openWindow(welcomeScreen);
-});
 }
