5c5dcb14ea0893151ede872eec6c8aac3936e80c
Rajan KC <rajankckc7@gmail.com>
2026-07-21 16:43:10 +0000

    Add Minecraft app icon and window functionality with styles

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function handleWindowTap(element) {
 }
 
 function addWindowTapHandling(element) {
-    if(element) {
+    if (element) {
         element.addEventListener("mousedown", () => handleWindowTap(element));
     }
 }
@@ -32,97 +32,107 @@ function selectIcon(element) {
 }
 
 function deselectIcon(element) {
-    if(element) {
+    if (element) {
         element.classList.remove("selected");
     }
     selectedIcon = undefined;
 }
 
-function unlockOS(e) {
-    e.preventDefault();
-    var welcomeScreen = document.querySelector("#welcome");
-    closeWindow(welcomeScreen);
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-    var welcomeScreen = document.querySelector("#welcome");
-    var welcomeScreenClose = document.querySelector("#welcomeclose");
-    var welcomeScreenOpen = document.querySelector("#welcomeopen");
-    
-    var notesScreen = document.querySelector("#notes");
-    var notesScreenClose = document.querySelector("#notesclose");
+    var welcomeWindow = document.querySelector("#welcome");
+    var welcomeClose = document.querySelector("#welcomeclose");
+    var welcomeOpen = document.querySelector("#welcomeopen");
+
+    var notesWindow = document.querySelector("#notes");
+    var notesClose = document.querySelector("#notesclose");
     var notesIcon = document.querySelector("#notesicon");
 
-    if (welcomeScreen) {
-        dragElement(welcomeScreen);
-        addWindowTapHandling(welcomeScreen);
+    var minecraftWindow = document.querySelector("#minecraft");
+    var minecraftClose = document.querySelector("#minecraftclose");
+    var minecraftIcon = document.querySelector("#minecrafticon");
+
+    [welcomeWindow, notesWindow, minecraftWindow].forEach((windowElement) => {
+        if (windowElement) {
+            dragElement(windowElement);
+            addWindowTapHandling(windowElement);
+        }
+    });
+
+    if (welcomeOpen && welcomeWindow) {
+        welcomeOpen.addEventListener("click", () => openWindow(welcomeWindow));
     }
 
-    if (notesScreen) {
-        dragElement(notesScreen);
-        addWindowTapHandling(notesScreen);
+    if (welcomeClose && welcomeWindow) {
+        welcomeClose.addEventListener("click", (e) => {
+            e.stopPropagation();
+            closeWindow(welcomeWindow);
+        });
     }
 
-    if (notesIcon && notesScreen) {
+    if (notesIcon && notesWindow) {
         notesIcon.addEventListener("click", (e) => {
-            openWindow(notesScreen);
-        });
-    }
-
-    if (welcomeScreenClose && welcomeScreen) {
-        welcomeScreenClose.addEventListener("click", (e) => {
             e.stopPropagation();
-            closeWindow(welcomeScreen);
+            selectIcon(notesIcon);
+            openWindow(notesWindow);
         });
     }
 
-    if (welcomeScreenOpen && welcomeScreen) {
-        welcomeScreenOpen.addEventListener("click", () => {
-            openWindow(welcomeScreen);
-        });
-    }
-
-    if (notesScreenClose && notesScreen) {
-        notesScreenClose.addEventListener("click", (e) => {
+    if (notesClose && notesWindow) {
+        notesClose.addEventListener("click", (e) => {
             e.stopPropagation();
-            closeWindow(notesScreen);
+            closeWindow(notesWindow);
+        });
+    }
+
+    if (minecraftIcon && minecraftWindow) {
+        minecraftIcon.addEventListener("click", (e) => {
+            e.stopPropagation();
+            selectIcon(minecraftIcon);
+            openWindow(minecraftWindow);
+        });
+    }
+
+    if (minecraftClose && minecraftWindow) {
+        minecraftClose.addEventListener("click", (e) => {
+            e.stopPropagation();
+            closeWindow(minecraftWindow);
         });
     }
 });
 
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  var targetHeader = document.getElementById(elmnt.id + "header");
-  
-  if (targetHeader) {
-    targetHeader.onmousedown = dragMouseDown;
-  } else {
-    elmnt.onmousedown = dragMouseDown;
-  }
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    var targetHeader = document.getElementById(elmnt.id + "header");
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    if (e.target.className === "close" || e.target.className === "minimize" || e.target.className === "maximize") return;
-    e.preventDefault();
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.onmousemove = elementDrag;
-  }
+    if (targetHeader) {
+        targetHeader.onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
+    function dragMouseDown(e) {
+        e = e || window.event;
+        if (e.target.className === "close" || e.target.className === "minimize" || e.target.className === "maximize") return;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
 
-  function closeDragElement() {
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
